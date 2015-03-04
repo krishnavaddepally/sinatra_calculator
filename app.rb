a735931135054764bc7dc74f2804efc603321246
Dan Pickett <dan.pickett@launchware.com>
2015-03-04 10:26:20 -0500

    simple calculator app

--- app.rb
+++ app.rb
@@ -11,6 +11,24 @@ Dir[File.join(File.dirname(__FILE__), 'lib', '**', '*.rb')].each do |file|
 end
 
 get '/' do
-  @title = "Hello World"
   erb :index
+end
+
+post '/calculate' do
+  result = nil
+  first_number = params[:first_number].to_i
+  second_number = params[:second_number].to_i
+  case params[:operation]
+  when "+"
+    result = first_number + second_number
+  when "-"
+    result = first_number - second_number
+  when "*"
+    result = first_number * second_number
+  when "/"
+    result = first_number / second_number
+  else
+    result = nil
+  end
+  erb :index, locals: { result: result }
 end
