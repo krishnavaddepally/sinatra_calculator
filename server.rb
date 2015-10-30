22e44dfab7fc12c796aea17b14c0b12eaeb4190a
cmkoller <cmkoller01@gmail.com>
2015-10-30 13:11:53 -0400

    Update to remove locals hash [#107048462]

---
+++ server.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'pry'
+
+get '/' do
+  erb :index
+end
+
+post '/calculate' do
+  @result = nil
+  first_number = params[:first_number].to_i
+  second_number = params[:second_number].to_i
+  case params[:operation]
+  when "+"
+    @result = first_number + second_number
+  when "-"
+    @result = first_number - second_number
+  when "*"
+    @result = first_number * second_number
+  when "/"
+    @result = first_number / second_number
+  else
+    @result = nil
+  end
+  erb :index
+end
